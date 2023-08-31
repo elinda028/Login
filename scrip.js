8413f25ffe7f28d8f9fd59d31e61449cbe019239
elinda028 <141394734+elinda028@users.noreply.github.com>
2023-08-31 11:02:47 +0700

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -11,19 +11,3 @@ signInButton.addEventListener('click',() => {
 document.querySelector('.btn1').addEventListener('click', () => {
     window.location.href = "tableAdmin.html"
 })
-
-var email = document.forms['form']['Email'];
-var password = document.forms['form']['password'];
-var usename = document.forms['form']['Usename'];
-var email_eror = document.getElementById('email_eror');
-var user_eror = document.getElementById('user_eror');
-var pass_eror = document.getElementById('pass_eror');
-
-function validated() {
-    if (email.value.length < 9> {
-        email.style.border = "1px solid red";
-        email_eror.style.display = "block"
-        email.focus();
-        return false;
-    }
-}
